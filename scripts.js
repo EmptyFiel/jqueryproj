2221982f5d67af2a70d04ba48eaf1179573aa2b7
EmptyFiel <166894101+EmptyFiel@users.noreply.github.com>
2025-04-09 21:22:29 -0700

    pyramid

--- scripts.js
+++ scripts.js
@@ -1,13 +1,38 @@
 $(document).ready(function(){
-	let isRotate = false;
-	$('button').on('click', function(){
-		isRotate = !isRotate;
-		$('#parent').attr('data-rotate', isRotate);
-	});
-	$('.grandChild').on('mouseover', function(){
-		$(this).css('transform', 'translate3d(0px, 0px, 20px)');
-	});
-	$('.grandChild').on('mouseout', function(){
+
+	function checkNear(x, y) {
+		x = +x;
+		y = +y;
+		for (let i = -1; i < 2; i++) {
+			for (let j = -1; j < 2; j++) {
+				if (i === 0 && j === 0) continue;
+				let col = x + i;
+				let row = y + j;
+				if (col >= 0 && row >= 0) {
+					$(`.grandChild[data-row="${row}"][data-col="${col}"]`).css('transform', 'translate3d(0px, 0px, 30px)');
+				}
+			}
+		}
+	}
+
+	function removeNear(){
+		for (let i = 0; i < 10; i++) {
+			for (let j = 0; j < 10; j++) {
+				let col = i;
+				let row = j;
+				$(`.grandChild[data-row="${row}"][data-col="${col}"]`).css('transform', 'translate3d(0px, 0px, 0px)');
+			}
+		}
+	}
+
+	$('.grandChild').on('mouseover', function() {
+		$(this).css('transform', 'translate3d(0px, 0px, 50px)');
+		let y = $(this).attr('data-row');
+		let x = $(this).attr('data-col');
+		checkNear(x, y);
+
+	}).on('mouseout', function(){
 		$(this).css('transform', 'translate3d(0px, 0px, 0px)');
+		removeNear();
 	});
 });
