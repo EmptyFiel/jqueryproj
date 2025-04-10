b31e08e8702f6426ba4b2dfe668777ea1958e66c
EmptyFiel <166894101+EmptyFiel@users.noreply.github.com>
2025-04-09 21:45:12 -0700

    failed shadows

--- scripts.js
+++ scripts.js
@@ -9,18 +9,21 @@ $(document).ready(function(){
 				let col = x + i;
 				let row = y + j;
 				if (col >= 0 && row >= 0) {
-					$(`.grandChild[data-row="${row}"][data-col="${col}"]`).css('transform', 'translate3d(0px, 0px, 30px)');
+					let element = $(`.grandChild[data-row="${row}"][data-col="${col}"]`);
+					element.css('transform', 'translate3d(0px, 0px, 30px)');
 				}
 			}
 		}
 	}
 
+
 	function removeNear(){
 		for (let i = 0; i < 10; i++) {
 			for (let j = 0; j < 10; j++) {
-				let col = i;
+				let col = i ;
 				let row = j;
-				$(`.grandChild[data-row="${row}"][data-col="${col}"]`).css('transform', 'translate3d(0px, 0px, 0px)');
+					let element = $(`.grandChild[data-row="${row}"][data-col="${col}"]`);
+					element.css('transform', 'translate3d(0px, 0px, 0px)');
 			}
 		}
 	}
